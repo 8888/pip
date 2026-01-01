82d19688e270e2add07b26917729750d0de529cc
8888 <leecostello1@gmail.com>
2026-01-01 11:05:16 -0500

    add gradle build

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    val kotlinVersion = "1.9.22"
+    id("org.jetbrains.kotlin.jvm") version kotlinVersion
+    id("com.microsoft.azure.azurefunctions") version "1.16.1"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.microsoft.azure.functions:azure-functions-java-library:3.1.0")
+    implementation(kotlin("stdlib"))
+    
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
+    testImplementation("org.mockito:mockito-core:5.8.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+azurefunctions {
+    appName = "pip-tracker"
+    resourceGroup = "java-functions-group"
+    region = "westus"
+    // runtime configuration removed for now
+    localDebug = "transport=dt_socket,server=y,suspend=n,address=5005"
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "21"
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
